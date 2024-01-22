8a2acaa2143b0e3c1055de0b9ce27c656c4989f1
Shodibek Musaev <shodik91@gmail.com>
2024-01-22 16:34:48 +0500

    Когда в блоке нет видео, скрипт ругается

--- src/js/apartmentsVideo.js
+++ src/js/apartmentsVideo.js
@@ -7,29 +7,32 @@ export default function apartmentsVideo() {
 
     elements.forEach(element => {
         const video = element.querySelector('video');
-        element.addEventListener('mouseenter', () => {
-            // video.pause();
-            video.currentTime = 0;
-            video.play();
-            element.classList.add('video-shown');
-        });
 
-        element.addEventListener('mouseleave', () => {
-            video.pause();
-            element.classList.remove('video-shown');
-        });
-
-        element.addEventListener('click', event => {
-            event.preventDefault();
-            if (video.playing) {
-                video.pause();
-                element.classList.remove('video-shown');
-            } else {
+        if (video != null) {
+            element.addEventListener('mouseenter', () => {
                 // video.pause();
                 video.currentTime = 0;
                 video.play();
                 element.classList.add('video-shown');
-            }
-        });
+            });
+
+            element.addEventListener('mouseleave', () => {
+                video.pause();
+                element.classList.remove('video-shown');
+            });
+
+            element.addEventListener('click', event => {
+                event.preventDefault();
+                if (video.playing) {
+                    video.pause();
+                    element.classList.remove('video-shown');
+                } else {
+                    // video.pause();
+                    video.currentTime = 0;
+                    video.play();
+                    element.classList.add('video-shown');
+                }
+            });
+        }
     });
 }
