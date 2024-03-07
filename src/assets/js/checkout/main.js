da29090ed32aaa4a0ccbdec382b4daca48715dd7
Shodibek Musaev <shodik91@gmail.com>
2024-03-07 09:14:53 +0500

    fix 07.03

--- src/assets/js/checkout/main.js
+++ src/assets/js/checkout/main.js
@@ -51,4 +51,27 @@ counters.forEach(counter => {
     });
 });
 
-customSelect('select');
+const customTrigger = document.querySelectorAll('.custom-trigger');
+
+document.addEventListener('click', function(event) {
+    let isClickInsideBlock = false;
+
+    for (let block of customTrigger) {
+        if (block.contains(event.target)) {
+            isClickInsideBlock = true;
+            break;
+        }
+    }
+
+    if (!isClickInsideBlock) {
+        for (let block of customTrigger) {
+            block.classList.remove('is-open');
+        }
+    }
+});
+
+for (let block of customTrigger) {
+    block.addEventListener('click', function() {
+        this.classList.toggle('is-open');
+    });
+}
