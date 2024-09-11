23f863a25af4c24f06f441ad649ae2937e5d6038
Shodibek Musaev <shodik91@gmail.com>
2024-09-12 01:38:56 +0500

    add disabled promocode submit

--- src/js/promocode.js
+++ src/js/promocode.js
@@ -1,13 +1,21 @@
 export default function eventPromoInput() {
     const promocodeInput = document.querySelectorAll('.js-promocode');
+    const promoSubmitButton = document.querySelector('.booking__checkout-form-submit');
 
     promocodeInput.forEach(element => {
         const promocodeButton = element.parentElement.querySelector('.booking__checkout-promocode-button');
         element.addEventListener('input', () => {
             if (element.value.trim() !== '') {
                 promocodeButton.classList.add('is-active');
+                if (promoSubmitButton) {
+                    promoSubmitButton.disabled = true;
+                }
             } else {
                 promocodeButton.classList.remove('is-active');
+
+                if (promoSubmitButton) {
+                    promoSubmitButton.disabled = false;
+                }
             }
         });
     });
